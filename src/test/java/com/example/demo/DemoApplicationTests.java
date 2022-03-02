12d90741208a0de38e8cf41a9b741dac8d9a8d9a
aymen1102 <baroudi.aymen@gmail.com>
2022-03-02 16:29:32 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	Calculator calculator = new Calculator();
+
+	@Test
+	void itShouldAddNumbers() {
+		//given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		//when
+		int result = calculator.add(numberOne,numberTwo);
+
+		// then
+		int expected = 50;
+		Assertions.assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator{
+		int add(int a,int b){
+			return a+b;
+		}
+	}
+}
---
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.student;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailExists() {
+        // given
+        String email = "amina@gmail.com";
+        Student student = new Student(
+                "Amina",
+                email,
+                Gender.FEMALE
+        );
+        underTest.save(student);
+
+        // when
+        Boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        Assertions.assertThat(expected).isTrue();
+
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExists() {
+        // given
+        String email = "amina@gmail.com";
+
+        // when
+        Boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        Assertions.assertThat(expected).isFalse();
+
+    }
+}
